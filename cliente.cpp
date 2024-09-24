ad04d160e8bff6509c5d8f4c8f070e08362a8529
Mateus Rotta <mateuseduardo779@gmail.com>
2024-09-23 22:06:34 -0300

    Implementei a inicialização do winsock no lado do cliente/caixa

---
+++ cliente.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+#define PORT 8080
+using namespace std;
+
+int main(){
+
+    // Inicia o Winsock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0){
+        cerr << "Erro ao inicializar o Winsock" << endl;
+        return -1;
+    }
+
+    // Criação do socket
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        cerr << "Erro ao criar socket" << endl;
+        WSACleanup(); 
+        return -1;
+    }
+
+    // Configuração do servidor
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // Tentativa de conexão com o servidor
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+        cerr << "Erro ao conectar ao servidor" << endl;
+        cerr << "Código de erro: " << WSAGetLastError() << endl;
+        closesocket(sock);
+        WSACleanup();
+        return -1;
+    }
+
+return 0;
+
+}
