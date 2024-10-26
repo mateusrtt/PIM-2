230b57c17bd93286a3359a84a46cf27d1c10c8d7
Mateus Rotta <mateuseduardo779@gmail.com>
2024-10-26 18:17:11 -0300

    Adicionei comentarios ao produto.h

--- produto/produto.h
+++ produto/produto.h
@@ -2,12 +2,18 @@
 #define PRODUTO_H
 #include <string>
 
+/**
+ * @brief Estrutura que representa um produto.
+ * 
+ * A estrutura `Produto` contém informações sobre um item, incluindo
+ * seu nome, preços e quantidades.
+ */
 struct Produto {
-    std::string nome; 
-    float precoPorKg; 
-    float precoPorUnidade; 
-    float quantidadeKg; 
-    int quantidadeUnidade;
+    std::string nome; ///< Nome do produto.
+    float precoPorKg; ///< Preço por quilograma do produto.
+    float precoPorUnidade; ///< Preço por unidade do produto.
+    float quantidadeKg; ///< Quantidade do produto em quilogramas.
+    int quantidadeUnidade; ///< Quantidade do produto em unidades.
 };
 
 #endif
